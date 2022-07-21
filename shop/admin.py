95917ace0ca0acab3d332d86cc482cf36dd49bbd
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-21 14:52:36 +0900

    added admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import  Product, PostTag, Detail, Detail_tag
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_field = {'slug':('name',)}
+    list_per_page = 20
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(PostTag)
+
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    list_editable = ['price']
+
+admin.site.register(Detail, DetailAdmin)
+
+admin.site.register(Detail_tag)
