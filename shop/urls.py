41801e3679b93ad2fdbdce8b64552db6a43cc3c2
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-22 15:53:45 +0900

    add p_detail.html, p_list.html, urls.py, views.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'shop'
 
-urlpatterns = [
-    path('', views.index, name="index"),
+urlpatterns = [path('', views.all_products, name='all_product'),
+               path('<slug:detail_slug>/', views.Detail_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -12,10 +12,65 @@ def my_customized_server_error(request, template_name='500.html'):
 
 from django.shortcuts import render
 
+from shop.models import Product, Detail, PostTag
 
-def index(request):
-    context = {
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
+from django.db.models import Q
+
+
+
+# import code
+# console = code.InteractiveConsole(locals=locals()) # <- locals=locals() が重要
+# console.interact()
+
+
+def all_products(request):
+    products_list = Product.valid_objects.all()
+    details = Detail.objects.all()
+    posts = PostTag.objects.all()
+
+    paginator = Paginator(products_list, per_page=12)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
+    data = {'products': products,
+            'details': details,
+            'posts': posts,
+            'apple': 'りんご',
     }
-    return render(request, 'base.html', context)
+        
+    return render(request, 'product_list.html', data)
 
+# 新しい商品detailのやつ
+def Detail_detail(request, detail_slug):
+    try:
+        product = Detail.objects.get(slug=detail_slug)
+    except Exception as e:
+        raise e
+    
+    products = Product.objects.all()
+
+    query = None
+    detail = None
+
+    if 'd' in request.GET:
+        query = request.GET.get('d')
+        detail = Detail.objects.all().order_by('price').filter(Q(subtag__name__contains=query))
+
+    
+    data = {'product': product,
+            'apple': 'りんご',
+            'detail': detail,
+            'query': query,
+            'products': products,
+    }
+        
+    return render(request, 'product_detail.html', data)
