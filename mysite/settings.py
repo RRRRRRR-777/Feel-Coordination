239dad5a32b4b9fbd4c04e287d76215ab15eb1af
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-25 09:32:50 +0900

    added cart:models.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,13 +30,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'shop',
+    'search',
+    'cart',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
