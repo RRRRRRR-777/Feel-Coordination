428dc17523989883e39eb8e806c95886465494ed
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-25 09:14:07 +0900

    added search:urls.py views.py

--- search/views.py
+++ search/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from shop.models import Product, Detail
+from django.db.models import Q
 
-# Create your views here.
+
+def search_result(request):
+    products = None
+    details = None
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET.get('q')
+        products = Product.objects.all().filter(Q(name__contains=query) | Q(tags__name__contains=query))
+        details = Detail.objects.all().filter(Q(name__contains=query) | Q(tags__name__contains=query))
+
+    return render(request, 'search.html', {'query': query, 'products': products, 'details': details})
