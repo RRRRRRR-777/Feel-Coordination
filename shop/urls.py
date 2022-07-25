8f86bd2bedab86262945d419f357734ec599e5c2
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-25 14:54:46 +0900

    added product_detail.html modified shop:urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [path('', views.all_products, name='all_product'),
-               path('<slug:detail_slug>', views.Detail_detail, name='product_detail'),
+               path('<slug:detail_slug>/', views.Detail_detail, name='product_detail'),
 ]
